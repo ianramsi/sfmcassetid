f62f78002b802f0b7452b1ecb7ac48d03b92faa3
Suplo Silpo <ian.ramsianto@gmail.com>
2023-04-17 12:39:50 +0700

    Update assetmc.js

--- assetmc.js
+++ assetmc.js
@@ -4,7 +4,7 @@
 try {
     // authenticate to SFMC get access token
     console.log('Executing my Stupid Code that I have been working for hours!!!!!');
-    var authEndpoint = 'https://xxxxxxxxxx.auth.marketingcloudapis.com/' //provide API endpoint
+    var authEndpoint = 'https://xxxxxxxxxx.auth.marketingcloudapis.com' //provide API endpoint
     var payload = {
         client_id: "xxxxxxxxxxxxx",
         client_secret: "xxxxxxxxxxxxx",
