99b5d4a93a8c6594229d837872c46c21a1e852f6
Suplo Silpo <ian.ramsianto@gmail.com>
2023-04-17 09:18:22 +0700

    Create assetmc.js

---
+++ assetmc.js
@@ -0,0 +1,57 @@
+<script runat = "server">
+    Platform.load ("Core", "1");
+
+try {
+    // authenticate to SFMC get access token
+    console.log('Executing my Stupid Code that I have been working for hours!!!!!');
+    var authEndpoint = 'https://mcnnlkd7mzpd-bycw4jyx87ymq30.auth.marketingcloudapis.com/' //provide API endpoint
+    var payload = {
+        client_id: "u9isifkofpkuv3gneavm40pl",
+        client_secret: "PwZDtHIRPEfzJkXi3mRSqAUT",
+        account_id: "534004987",
+        grant_type: "client_credentials"
+
+    };
+    var url = authEndPoint + '/v2/token';
+    var contentType = 'application/json';
+
+    var accessTokenRequest = HTTP.Post(url, contentType, Stringify(payload));
+    If (accessTokenRequest == 200) {
+        var tokenResponse = Platform.Function.ParseJSON(accessTokenRequest.Response[0]);
+        var accessToken = tokenResponse.access_token; //get token
+        var rest_instance_url = tokenResponse.rest_instance_url; //get rest url
+    };
+
+    //make api call via request
+    if (access_token != null) {
+        var assetURL = rest_instance_url + "/asset/v1/content/assets?$page=1"; //initial url
+        var headerNames = ["Authorization"];
+        var headerValues = ["Bearer" + accessToken];
+        var assetResponse = Platform.Function.ParseJSON(String(assetResponse.Content));
+        var assetAccount = finalResponse.count;
+        var pageCount = finalResponse.page;
+        var pageSize = finalResponse.pageSize;
+        var iteraterows = Math.ceil(assetCount / pageSize) + 1; //Formula to get how many times we need to call the content API
+        for (var j = 1; j < iteraterows; j++) {
+            var assetURLFinal = rest_instance_url + "/asset/v1/content/assets?$page=" +j; //Iterating the pages dynamically
+            var headerNames = ["Authorization"];
+            var headerValues = ["Bearer " + accessToken];
+            var assetresponseFinal = HTTP.Get(assetURLFinal, headerNames, headerValues);
+            var finalResponseFinal = Platform.Function.ParseJSON(String(assetresponseFinal.Content));
+            for (var i = 0; i < finalResponseFinal.items.length; i++) {
+                var displayName = finalResponseFinal.items[i].assetType.displayName;
+                if (displayName == "Image") { //checking if the type is Image
+                    var assetId = finalResponseFinal.items[i].id;
+                    var publishedURL = finalResponseFinal.items[i].fileProperties.publishedURL;
+                    var fileName = finalResponseFinal.items[i].fileProperties.fileName;
+                    var rows = Platform.Function.UpsertDE("DE_Asset_ID", ["Asset_Id"],[id] ,["Asset_URL"], [publishedURL], ["Asset_Name"], [fileName]); //upserting the Image URL Link, ID and file name
+                }
+            }
+        }
+
+    }
+    
+} catch (error) {
+    Write(Stringify(error));
+}
+</script>
