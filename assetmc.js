04e4cb38cede4ad542b3fa378cbe9beafb403739
Suplo Silpo <ian.ramsianto@gmail.com>
2023-04-19 17:00:10 +0700

    Update assetmc.js

--- assetmc.js
+++ assetmc.js
@@ -22,29 +22,37 @@ try {
 
     //make api call via request
     if (access_token != null) {
-        var assetURL = rest_instance_url + "/asset/v1/content/assets?$page=1"; //initial url
-        var headerNames = ["Authorization"];
-        var headerValues = ["Bearer" + accessToken];
-        var assetResponse = Platform.Function.ParseJSON(String(assetResponse.Content));
-        var assetAccount = finalResponse.count;
-        var pageCount = finalResponse.page;
-        var pageSize = finalResponse.pageSize;
-        var iteraterows = Math.ceil(assetCount / pageSize) + 1; //Formula to get how many times we need to call the content API
-        for (var j = 1; j < iteraterows; j++) {
-            var assetURLFinal = rest_instance_url + "/asset/v1/content/assets?$page=" +j; //Iterating the pages dynamically
+            var assetURL = rest_instance_url+"asset/v1/content/assets?page=1"; //get URL for asset
             var headerNames = ["Authorization"];
             var headerValues = ["Bearer " + accessToken];
-            var assetresponseFinal = HTTP.Get(assetURLFinal, headerNames, headerValues);
-            var finalResponseFinal = Platform.Function.ParseJSON(String(assetresponseFinal.Content));
-            for (var i = 0; i < finalResponseFinal.items.length; i++) {
-                var displayName = finalResponseFinal.items[i].assetType.displayName;
-                if (displayName == "Image") { //checking if the type is Image
-                    var assetId = finalResponseFinal.items[i].id;
-                    var publishedURL = finalResponseFinal.items[i].fileProperties.publishedURL;
-                    var fileName = finalResponseFinal.items[i].fileProperties.fileName;
-                    var rows = Platform.Function.UpsertDE("DE_Asset_ID", ["Asset_Id"],[id] ,["Asset_URL"], [publishedURL], ["Asset_Name"], [fileName]); //upserting the Image URL Link, ID and file name
+            var assetResponse = HTTP.Get(assetURL, headerNames, headerValues);
+            var finalResponse = Platform.Function.ParseJSON(String(assetResponse.Content));
+            var assetCount = finalResponse.count;
+            var pageCount = finalResponse.page;
+            var pageSize = finalResponse.pageSize;
+			
+			var  iteraterows = Math.ceil (assetCount / pageSize) + 1; //formula to get how many times to call the the content api
+	
+            for (var y = 1; y < iteraterows; y++) {
+                var assetURLFinal = rest_instance_url+"asset/v1/content/assets?page="+y; //iterate content pages dynamically								
+                var headerNames = ["Authorization"];
+                var headerValues = ["Bearer " + accessToken];
+                var assetresponseFinal = HTTP.Get(assetURLFinal, headerNames, headerValues); //call API HTTP Get
+                var finalResponseFinal = Platform.Function.ParseJSON(String(assetresponseFinal.Content));
+
+                for( var i = 0; i < finalResponseFinal.items.length; i++) {
+                    var displayName = finalResponseFinal.items[i].assetType.displayName;
+                    if (displayName == "Image") {                        
+                        var assetId = finalResponseFinal.items[i].id;
+                        var publishedURL = finalResponseFinal.items[i].fileProperties.publishedURL;
+                        var fileName = finalResponseFinal.items[i].fileProperties.fileName;
+                        var rows = Platform.Function.UpsertDE("DE_Asset_ID", ["Asset_Id"], [assetId], ["Asset_Name", "Asset_URL"], [fileName, publishedURL]); //upserting the data
+                    }
+
                 }
+
             }
+
         }
 
     }
